e7428e95426de88a9064e5800e28bc20a01e0324
SuperSuperMoon <47732974+SuperSupermoon@users.noreply.github.com>
2021-10-19 20:11:06 +0900

    Create coco.sh

---
+++ coco.sh
@@ -0,0 +1,12 @@
+# coco 2017 dataset download
+
+mkdir coco
+cd coco
+
+wget -c http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+wget -c http://images.cocodataset.org/zips/train2017.zip
+wget -c http://images.cocodataset.org/zips/val2017.zip
+
+unzip annotations_trainval2017.zip
+unzip train2017.zip
+unzip val2017.zip
